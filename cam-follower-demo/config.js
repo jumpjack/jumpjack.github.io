66bf57aff1448a49d0a586fd0375da5225c72d6d
jumpjack <jumpjack@libero.it>
2021-11-13 18:39:15 +0100

    first

---
+++ cam-follower-demo/config.js
@@ -0,0 +1,9 @@
+config = {
+	"pathWidth": 2,
+	"lineColor": "#000000",
+	"fillColor": "#888888",
+	"pointsColor": "#FFFF00",
+	"flatpathColor": "#000000",
+	"flatpathPointsColor": "#FFFF00",
+	"flatpathWidth": 2
+}
