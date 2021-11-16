9c8934fd8015e02f80ab5a6e019dd73cbf22286f
jumpjack <jumpjack@libero.it>
2021-11-16 10:00:31 +0100

    V. 0.1.3
    // Added flat path markers
    // Added display of rotation angle value
    // Added slider for manual rotation
    // Added import/export SVG
    // Grouped import/export buttons
    // Cleaned up code
    // Increased resolution of circular path (indepdendent of flat path resolution)

--- cam-follower-demo/config.js
+++ cam-follower-demo/config.js
@@ -5,5 +5,12 @@ config = {
 	"pointsColor": "#FFFF00",
 	"flatpathColor": "#000000",
 	"flatpathPointsColor": "#FFFF00",
-	"flatpathWidth": 2
+	"flatpathWidth": 1,
+	"normalColor" : "#000000",
+	"normalWidth" : 3,
+	"followerLength" : 90,
+	"followerColor" : "#4444FF",
+	"followerWidth" : 10,
+	"initialFollowerOffsetX" : 0,
+	"initialFollowerOffsetY" : 0
 }
